b1ffd2c9cee75cd979e5e80a8284d6eebf9da02f
Slemmerten <32939184+Slemmerten@users.noreply.github.com>
2018-04-06 12:39:14 +0200

    Adding Files

---
+++ Fasetto.Word/DataModels/ApplicationPage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// A Page of the application
+/// </summary>
+namespace Fasetto.Word
+{
+    /// <summary>
+    /// Initial Login page
+    /// </summary>
+    enum ApplicationPage
+    {
+        Login = 0,
+    }
+}
---
+++ Fasetto.Word/ViewModel/Base/BaseViewModel.cs
@@ -0,0 +1,26 @@
+﻿using PropertyChanged;
+using System.ComponentModel;
+
+namespace Fasetto.Word
+{
+    /// <summary>
+    /// Base view model that fires property changed events as needed
+    /// </summary>
+    
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// The event that is fired when any child property changes its value
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged = (sender, e) => { };
+
+        /// <summary>
+        /// Call this to fire a <see cref="PropertyChanged"/>
+        /// </summary>
+        /// <param name="name"></param>
+        public void OnPropertyChanged(string name)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
