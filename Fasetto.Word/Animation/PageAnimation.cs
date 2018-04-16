be0a144c547c09f8c43b214ffeaefc69bcfdffdc
Slemmerten <32939184+Slemmerten@users.noreply.github.com>
2018-04-16 18:31:00 +0200

    added animations

---
+++ Fasetto.Word/Animation/PageAnimation.cs
@@ -0,0 +1,21 @@
+﻿namespace Fasetto.Word
+{
+    /// <summary>
+    /// Style of page animations for appearing/disappearing
+    /// </summary>
+    public enum PageAnimation
+    {
+        /// <summary>
+        /// No animation
+        /// </summary>
+        None = 0,
+        /// <summary>
+        /// The page slides in and fades in from the right
+        /// </summary>
+        SlideAndFadeInFromRight = 1,
+        /// <summary>
+        /// The page slides out and fades out to the left
+        /// </summary>
+        SlideAndFadeOutToLeft = 2,
+    }
+}
---
+++ Fasetto.Word/Pages/BasePage.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+
+namespace Fasetto.Word
+{
+    /// <summary>
+    /// a base page for all pages to gain base functionality
+    /// </summary>
+    public class BasePage : Page
+    {
+        #region Public Properties
+        /// <summary>
+        /// The animation that plays when the page first loads
+        /// </summary>
+        public PageAnimation PageLoadAnimation { get; set; } = PageAnimation.SlideAndFadeInFromRight;
+
+        /// <summary>
+        /// The animation that plays when the page unloads
+        /// </summary>
+        public PageAnimation PageUnLoadAnimation { get; set; } = PageAnimation.SlideAndFadeOutToLeft;
+
+        /// <summary>
+        /// How long the animation takes to complete
+        /// </summary>
+        public float SlideSeconds { get; set; } = 0.8f;
+
+
+        #endregion
+
+        #region Constructor
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public BasePage()
+        {
+            //If we are animating in, hide to begin with
+            if (this.PageLoadAnimation != PageAnimation.None)
+                this.Visibility = Visibility.Collapsed;
+
+            //Listen out for the page loading
+            this.Loaded += BasePage_Loaded;
+        }
+        #endregion
+
+        #region Animation load / unload
+        /// <summary>
+        /// Once the page is loaded, perform any required animation
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void BasePage_Loaded(object sender, RoutedEventArgs e)
+        {
+            //Animate the page in
+            await AnimateIn();
+        }
+
+        /// <summary>
+        /// Animates the page in
+        /// </summary>
+        /// <returns></returns>
+        public async Task AnimateIn()
+        {
+            //Make sure we have something to do
+            if (this.PageLoadAnimation == PageAnimation.None)
+                return;
+
+            switch (this.PageLoadAnimation)
+            {
+                case PageAnimation.SlideAndFadeInFromRight:
+                    //Start the animation
+                    await this.SlideAndFadeInFromRight(this.SlideSeconds);
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Animates the page out
+        /// </summary>
+        /// <returns></returns>
+        public async Task AnimateOut()
+        {
+            //Make sure we have something to do
+            if (this.PageUnLoadAnimation == PageAnimation.None)
+                return;
+
+            switch (this.PageUnLoadAnimation)
+            {
+                case PageAnimation.SlideAndFadeOutToLeft:
+                    //Start the animation
+                    await this.SlideAndFadeOutToLeft(this.SlideSeconds);
+                    break;
+            }
+        }
+        
+        #endregion
+    }
+}
